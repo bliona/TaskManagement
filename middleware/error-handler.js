757d19f914956161a3f55e2049df427fb14fa5ef
bliona <lionamuhati16@gmail.com>
2024-02-28 16:21:51 -0800

    TaskManagement

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,17 @@
-const { CustomAPIError } = require('../errors/custom-error')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
-}
+const { CustomAPIError } = require('../errors/custom-error');
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  // Log the error for debugging purposes
+  console.error(err);
+
+  if (err instanceof CustomAPIError) {
+    // If it's a custom error, send a JSON response with the specific status code and message
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  // For generic errors, choose an appropriate status code (e.g., 500) and send a JSON response
+  return res.status(500).json({ error: 'Internal Server Error' });
+};
+
+module.exports = errorHandlerMiddleware;
+
